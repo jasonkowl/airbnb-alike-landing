985e325849d404f00014a7a0f6bfb25541c841d9
jason <jasonxgr@gmail.com>
2020-08-28 18:59:02 +0300

    Adding banner + Search

---
+++ src/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Search.css'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css';
+import {DateRangePicker} from 'react-date-range';
+
+// DATE PICKER COMPONENT 
+function Search() {
+    return (
+        <div className='search'>
+            
+        </div>
+    )
+}
+
+export default Search
