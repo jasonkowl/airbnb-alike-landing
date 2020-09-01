4340bf8987b597e448f9a653aa7d933aff1de41b
jasoncg <jasonxgr@gmail.com>
2020-09-01 20:03:47 +0300

    Added Cards, Footer, SearchPage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from "react";
-import "./App.css";
-import Home from "./Home";
-import Header from "./Header";
+import React from 'react';
+import './App.css';
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
+import SearchPage from './SearchPage'
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+
     // BEM
     <div className="app">
-      <Header />
-      <Home />
-      {/* Home */}
-      {/* Header */}
-
-      {/* Banner */}
-
-      {/* Search */}
-
-      {/* Cards */}
-
-      {/* Footer */}
-
-      {/* SearchPage */}
-      {/* Header */}
-      {/* ... */}
+      <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer />
+      </ Router>
     </div>
   );
 }
